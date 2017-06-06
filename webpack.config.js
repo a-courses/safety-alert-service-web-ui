e014d862e52c627d12d458bfdffd547765d45c2b
Sandeep <j.sandeepm@gmail.com>
2017-06-07 00:06:54 +0530

    Host access issue fixed

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = function makeWebpackConfig() {
 
         // Output path from the view of the page
         // Uses webpack-dev-server in development
-        publicPath: isProd ? '/' : 'http://localhost:8080/',
+        publicPath: isProd ? '/' : '/',
 
         // Filename for entry points
         // Only adds hash in build mode
@@ -223,6 +223,7 @@ module.exports = function makeWebpackConfig() {
      */
     config.devServer = {
         contentBase: './src/public',
+        disableHostCheck: true,
         stats: 'minimal'
     };
 
